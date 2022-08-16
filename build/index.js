1e2b639fb3f50dd7651fa49f89f16cf098f323b5
Gabriel Walsh <brotherclone@gmail.com>
2022-08-16 15:32:46 -0400

    new pattern

--- build/index.js
+++ build/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,60 +35,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const typeGraphQL = __importStar(require("type-graphql"));
 const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const contextProvider_1 = require("./contextProvider");
+const UserResolver_1 = require("./resolvers/UserResolver");
 const apiPath = "/" + process.env.API_PATH;
 const apiPort = process.env.PORT ? process.env.PORT : 3000;
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     const app = (0, express_1.default)();
     app.use(express_1.default.json());
-    const httpServer = (0, http_1.createServer)(app);
-    const typeDefs = (0, apollo_server_express_1.gql) `
-        type Board {
-            id: ID!
-            title: String!
-            description: String
-            path: String!
-        }
-        type Query {
-            boards: [Board!]!
-        }
-    `;
-    const resolvers = {
-        Query: {
-            boards: () => {
-                return prisma.board.findMany();
-            }
-        },
-    };
-    const apolloServer = new apollo_server_express_1.ApolloServer({
-        typeDefs,
-        resolvers,
+    const schema = yield typeGraphQL.buildSchema({
+        resolvers: [UserResolver_1.UserResolver]
     });
-    app.get(apiPath + "/test", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        const boardRes = yield prisma.board.findMany();
-        res.json(boardRes);
-    }));
-    app.post(apiPath + "/test", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        if (req.body) {
-            console.log(req.body);
-            const { title, description, path } = req.body;
-            const result = yield prisma.board.create({
-                data: {
-                    title,
-                    description,
-                    path
-                }
-            });
-            res.json(result);
-        }
-        else {
-            res.json({ message: "no request body" });
-        }
-    }));
+    const httpServer = (0, http_1.createServer)(app);
+    const apolloServer = new apollo_server_express_1.ApolloServer({ schema, context: contextProvider_1.prismaContext });
     yield apolloServer.start();
     apolloServer.applyMiddleware({
         app,
--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,28 @@
-import {createServer} from "http";
+import 'reflect-metadata'
+import * as typeGraphQL from 'type-graphql'
+import { createServer } from "http";
 import express from "express";
-import {ApolloServer, gql} from "apollo-server-express";
-import { PrismaClient } from "@prisma/client"
+import { ApolloServer } from "apollo-server-express";
+import { prismaContext } from "./contextProvider";
+import { GraphQLScalarType } from 'graphql'
+import { UserResolver } from './resolvers/UserResolver'
 
-const prisma = new PrismaClient()
 const apiPath = "/" + process.env.API_PATH
 const apiPort = process.env.PORT ? process.env.PORT : 3000
 
 const startServer = async () => {
     const app = express()
-    app.use(express.json());
+    app.use(express.json())
+    const schema = await typeGraphQL.buildSchema({
+        resolvers:[UserResolver]
+    })
     const httpServer = createServer(app)
-    const typeDefs = gql`
-        type Board {
-            id: ID!
-            title: String!
-            description: String
-            path: String!
-        }
-        type Query {
-            boards: [Board!]!
-        }
-    `;
-
-    const resolvers = {
-        Query: {
-            boards: () => {
-                return prisma.board.findMany()
-            }
-        },
-    };
-
-
-    const apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-    })
-
-    app.get(apiPath+"/test", async (req, res) =>{
-        const boardRes = await prisma.board.findMany()
-        res.json(boardRes)
-    })
-
-    app.post(apiPath+"/test", async (req, res) => {
-        if(req.body){
-            console.log(req.body)
-            const { title, description, path } = req.body
-            const result = await prisma.board.create({
-                data:{
-                    title,
-                    description,
-                    path
-                }
-            })
-            res.json(result)
-        }else{
-            res.json({message: "no request body"})
-        }
-
-    })
-
-
+    const apolloServer = new ApolloServer({schema, context: prismaContext})
     await apolloServer.start()
     apolloServer.applyMiddleware({
         app,
         path: apiPath
     })
-
     httpServer.listen({port: process.env.PORT || 4000}, () =>
         console.log('listening on ' +apiPort, 'serving at ' + apiPath )
     )
