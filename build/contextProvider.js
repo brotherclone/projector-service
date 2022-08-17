6009d7963544c8664c165cb82e9d297f64844215
Gabriel Walsh <brotherclone@gmail.com>
2022-08-17 17:28:46 -0400

    delete

--- build/contextProvider.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaContext = void 0;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.prismaContext = {
-    prisma: prisma
-};
