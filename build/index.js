a0e6f72d8817aaf24430997a7af3121f404461a3
Gabriel Walsh <brotherclone@gmail.com>
2022-08-18 08:30:40 -0400

    bones

--- build/index.js
+++ build/index.js
@@ -1,24 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
+const graphql_1 = require("@neo4j/graphql");
+const apollo_server_1 = require("apollo-server");
 const neo4j = require("neo4j-driver");
 const driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
-//
-//
-// const neo4jGraphQL = new Neo4jGraphQL({
-//     typeDefs,
-//     driver
-// });
-//
-// neo4jGraphQL.getSchema().then((schema) => {
-//     // Create ApolloServer instance to serve GraphQL schema
-//     const server = new ApolloServer({
-//         schema,
-//         context: { driverConfig: { database: 'neo4j' } }
-//     });
-//
-//     // Start ApolloServer
-//     server.listen().then(({ url }) => {
-//         console.log(`GraphQL server ready at ${url}`);
-//     });
-// });
+const typeDefs = (0, apollo_server_1.gql) `
+    type Movie {
+        title: String
+        actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+    }
+
+    type Actor {
+        name: String
+        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+    }
+`;
+const neo4jGraphQL = new graphql_1.Neo4jGraphQL({
+    typeDefs,
+    driver
+});
+neo4jGraphQL.getSchema().then((schema) => {
+    // Create ApolloServer instance to serve GraphQL schema
+    const server = new apollo_server_1.ApolloServer({
+        schema,
+        context: { driverConfig: { database: 'neo4j' } }
+    });
+    // Start ApolloServer
+    server.listen().then(({ url }) => {
+        console.log(`GraphQL server ready at ${url}`);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 require('dotenv').config()
 import {Neo4jGraphQL} from "@neo4j/graphql"
-import { ApolloServer } from "apollo-server"
-import typeDefs from './graphql/schema.graphql'
-import { UserResolver } from "./graphql/resolvers/UserResolver";
+import { ApolloServer, gql} from "apollo-server"
 
 const neo4j = require("neo4j-driver")
 const driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
-//
-//
-// const neo4jGraphQL = new Neo4jGraphQL({
-//     typeDefs,
-//     driver
-// });
-//
-// neo4jGraphQL.getSchema().then((schema) => {
-//     // Create ApolloServer instance to serve GraphQL schema
-//     const server = new ApolloServer({
-//         schema,
-//         context: { driverConfig: { database: 'neo4j' } }
-//     });
-//
-//     // Start ApolloServer
-//     server.listen().then(({ url }) => {
-//         console.log(`GraphQL server ready at ${url}`);
-//     });
-// });
+
+const typeDefs = gql`
+    interface Thing {
+        description: String!   
+    }
+    type CreativeWork implements Thing {
+        description: String!
+    }
+`;
+
+const neo4jGraphQL = new Neo4jGraphQL({
+    typeDefs,
+    driver
+});
+
+neo4jGraphQL.getSchema().then((schema) => {
+    const server = new ApolloServer({
+        schema,
+        context: { driverConfig: { database: 'neo4j' } }
+    });
+    server.listen().then(({ url }) => {
+        console.log(`GraphQL server ready at ${url}`);
+    });
+});
